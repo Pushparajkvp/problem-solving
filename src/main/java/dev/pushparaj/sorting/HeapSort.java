3811203d33d198569683d6e2f96985e2a1c1ddc7
pushparajkvp <pushparajkvp@gmail.com>
2020-02-17 01:27:08 +0530

    optimized heapsort

--- src/main/java/dev/pushparaj/sorting/HeapSort.java
+++ src/main/java/dev/pushparaj/sorting/HeapSort.java
@@ -4,47 +4,44 @@ import java.util.*;
 
 public class HeapSort {
 
-    static int[] tree; 
     static int heapSize = 0;
 
     public static void sort(int[] arr) {
-        tree = arr.clone();
-        heapSize = tree.length;
+        heapSize = arr.length;
         
         //Heapify
         for(int i = Math.max(0, (arr.length /2) - 1); i >= 0; i--) {
-            sink(tree, i);
+            sink(arr, i);
         }
-        for(int i = 0; i < arr.length; i++)
-            arr[i] = poll();
+        for(int i = 0; i < arr.length; i++) poll(arr);
     }
 
     private static void sink(int[] arr, int index) {
-        if(tree == null || tree.length == 0) return;
+        if(arr == null || arr.length == 0) return;
 
         while(true) {
             int left = (2 * index) + 1;
             int right = (2 * index) + 2;
 
-            int smallestIndex = left;
+            int biggerValueIndex = left;
 
-            if(right < heapSize && arr[right] < arr[smallestIndex]) smallestIndex = right;
+            if(right < heapSize && arr[right] > arr[biggerValueIndex]) biggerValueIndex = right;
 
-            if(smallestIndex >= heapSize || arr[smallestIndex] > arr[index]) break;
-            swap(arr, index, smallestIndex);
-            index = smallestIndex;
+            if(biggerValueIndex >= heapSize || arr[biggerValueIndex] < arr[index]) break;
+            swap(arr, index, biggerValueIndex);
+            index = biggerValueIndex;
         }
     }
 
-    private static int poll() {
-        if(tree == null || tree.length == 0) return -1;
+    private static int poll(int[] arr) {
+        if(arr == null || arr.length == 0) return -1;
 
         if(heapSize == 0) return -1;
 
-        int returnData = tree[0];
-        swap(tree, 0, heapSize - 1);
+        int returnData = arr[0];
+        swap(arr, 0, heapSize - 1);
         heapSize--;
-        sink(tree, 0);
+        sink(arr, 0);
         return returnData;
     }
 

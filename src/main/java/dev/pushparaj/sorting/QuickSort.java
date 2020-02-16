9bf45edd93232a9e8e18ebe9ef047e11dc505639
pushparajkvp <pushparajkvp@gmail.com>
2020-02-17 00:24:39 +0530

    added quicksort

--- src/main/java/dev/pushparaj/sorting/QuickSort.java
+++ src/main/java/dev/pushparaj/sorting/QuickSort.java
@@ -5,22 +5,23 @@ import java.util.*;
 public class QuickSort {
 
     private static int partition(int[] arr, int low, int high) {
-        int pivotValue = arr[low], left = low + 1, right = high;
+        int pivotValue = arr[high], left = low - 1, right = high;
+
         while(true) {
-            while(arr[left++] < pivotValue) if(left == high) break;
-            while(arr[right--] > pivotValue) if(right == low) break;
+            while(++left <= high && arr[left] < pivotValue);
+            while(--right >= low && arr[right] > pivotValue);
+
             if(left >= right) break;
             swap(arr, left, right);
         }
-        swap(arr, low, right);
-        return right;
+        swap(arr, high, left);
+
+        return left;
     }
 
     private static void quickSort(int[] arr, int low, int high) {
         if(low >= high) return;
-        System.out.println("Before Partition -> " + Arrays.toString(Arrays.copyOfRange(arr, low , high + 1)));
         int partitionIndex = partition(arr, low, high);
-        System.out.println("After Partition -> " + Arrays.toString(Arrays.copyOfRange(arr, low , high + 1)) + " Pivot Index -> " + (partitionIndex - low));
         quickSort(arr, low, partitionIndex - 1);
         quickSort(arr, partitionIndex + 1, high);
     }
@@ -30,7 +31,6 @@ public class QuickSort {
     }
 
     private static void swap(int[] arr, int firstIndex, int secondIndex) {
-        System.out.println("Swapping -> " + arr[firstIndex] + " and " + arr[secondIndex]);
         int temp = arr[firstIndex];
         arr[firstIndex] = arr[secondIndex];
         arr[secondIndex] = temp;

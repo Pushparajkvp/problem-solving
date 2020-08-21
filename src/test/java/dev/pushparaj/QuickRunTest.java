fef52678a7c015c49bed1ef7527bfcc026865f10
pushparajkvp <pushparajkvp@gmail.com>
2020-08-21 20:36:09 +0530

    quick run setup

---
+++ src/test/java/dev/pushparaj/QuickRunTest.java
@@ -0,0 +1,17 @@
+package dev.pushparaj.problems;
+
+import java.util.*;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import dev.pushparaj.BaseTest;
+import dev.pushparaj.*;
+
+public class QuickRunTest extends BaseTest {
+
+    @Test
+    public void run() {
+        QuickRun.run();
+    }
+}

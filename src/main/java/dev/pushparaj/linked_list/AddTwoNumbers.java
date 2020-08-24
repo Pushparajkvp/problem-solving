0c9d0197caf20c467485468b7dc4bee49c78d602
pushparajkvp <pushparajkvp@gmail.com>
2020-08-24 22:55:29 +0530

    added linkedlist number addition

---
+++ src/main/java/dev/pushparaj/linked_list/AddTwoNumbers.java
@@ -0,0 +1,44 @@
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+
+
+Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+*/
+class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode iterator = dummyHead;
+        
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            iterator.next  = new ListNode(sum % 10);
+            carry = sum/10;
+            
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+            iterator = iterator.next;
+        }
+        if(carry > 0)
+            iterator.next = new ListNode(carry);
+        
+        return dummyHead.next;
+    }
+}

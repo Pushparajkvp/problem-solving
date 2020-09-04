4c91de79c39fa76180a554d289cbb7ab2ced25bd
pushparajkvp <pushparajkvp@gmail.com>
2020-09-04 23:51:04 +0530

    added sorted linked list array problem

--- src/main/java/dev/pushparaj/linked_list/AddTwoNumbers.java
+++ src/main/java/dev/pushparaj/linked_list/AddTwoNumbers.java
@@ -1,3 +1,4 @@
+package dev.pushparaj.linked_list;
 /*
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 
---
+++ src/main/java/dev/pushparaj/linked_list/ArrayOfSortedLists.java
@@ -0,0 +1,95 @@
+package dev.pushparaj.linked_list;
+
+import java.util.*;
+/*
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 10^4
+0 <= lists[i].length <= 500
+-10^4 <= lists[i][j] <= 10^4
+lists[i] is sorted in ascending order.
+The sum of lists[i].length won't exceed 10^4.
+
+Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+*/
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+        if(lists == null || lists.length == 0) return null;
+        
+        int interval = 1;
+        
+        while(interval < lists.length) {
+            for(int it=0; it + interval<lists.length; it+=interval) {
+                lists[it] = merge2Lists(lists[it], lists[it+interval]);
+            }
+            interval*=2;
+        }
+        
+        return lists[0];
+    }
+    
+    private ListNode merge2Lists(ListNode l1, ListNode l2) {
+        ListNode tail = new ListNode();
+        ListNode dummy = tail;
+        
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                tail.next = new ListNode();
+                tail = tail.next;
+                tail.val = l1.val;
+                tail.next = null;
+                
+                l1 = l1.next;
+            } else {
+                tail.next = new ListNode();
+                tail = tail.next;
+                tail.val = l2.val;
+                tail.next = null;
+                
+                l2 = l2.next;
+            }
+        }
+        if(l1 != null) tail.next = l1;
+        if(l2 != null) tail.next = l2;
+        
+        return dummy.next;
+    }
+}
